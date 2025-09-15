1fdc7d99a2824144643c0dc561a59bc5503f727e
raisavar <raisavar@users.noreply.github.com>
2025-09-15 16:59:09 +0300

    improve: add error messages

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 33
 module.exports = API;
+
+
+// API module for EurekaChat
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 35
+module.exports = API;
