247e2d6073d608386223613682a9dd0cd10def40
raisavar <raisavar@users.noreply.github.com>
2026-01-03 03:17:03 +0200

    add: code cleanup

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 45
 module.exports = API;
+
+
+// API module for EurekaChat
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 46
+module.exports = API;
